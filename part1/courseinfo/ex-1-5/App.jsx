58d22900796cbc7d123ea69e43464d3ddebd5f2b
Oleksandra Antonova <werba2010@gmail.com>
2023-09-07 17:00:16 -0400

    Adding Ex.1.1-1.5

---
+++ part1/courseinfo/ex-1-5/App.jsx
@@ -0,0 +1,62 @@
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+} 
+
+const Content = ({course}) => {
+  return (
+    <>
+      <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />
+      <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />
+      <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />
+    </>
+  );
+}
+
+const Part = ({part, exercises}) => {
+  return (
+      <p>
+        {part} {exercises}
+      </p>
+  )
+}
+
+
+const Total = ({course}) => {
+  return (
+    <p>Number of exercises: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises }</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+ return (
+    <div>
+      <Header course={course.name} />
+     <Content course={course} />
+      <Total course={course}/>
+    </div>
+  )
+
+}
+
+
+export default App
