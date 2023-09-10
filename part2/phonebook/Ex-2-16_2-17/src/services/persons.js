076d156e0aea06d6060ef57f01d55de0fea31775
Oleksandra Antonova <werba2010@gmail.com>
2023-09-10 17:40:34 -0400

    updated persons.js link

--- part2/phonebook/Ex-2-16_2-17/src/services/persons.js
+++ part2/phonebook/Ex-2-16_2-17/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/personsdd';
+const baseUrl = 'http://localhost:3001/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
